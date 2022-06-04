5c5ca3c21d9c4753179e6aea308bc1b1e577c5ef
joonjang <wonjoonj@student.ubc.ca>
2022-06-03 22:38:48 -0700

    Initial Commit

---
+++ PomodoroStudy/PomodoroStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroStudyApp.swift
+//  PomodoroStudy
+//
+//  Created by Joon Jang on 2022-06-03.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
