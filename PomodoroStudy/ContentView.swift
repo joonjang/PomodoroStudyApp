f852c67e50f87a13323fbaab31f21bb5db64ab06
joonjang <wonjoonj@student.ubc.ca>
2022-08-19 22:58:15 -0700

    start and pause function established

--- PomodoroStudy/ContentView.swift
+++ PomodoroStudy/ContentView.swift
@@ -8,9 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var vm = ViewModel()
+    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        Text("Hello, world!")
+
+        VStack {
+            
+            HStack {
+                Button("Pomodoro") {
+                    vm.seconds = 25.0 * 60
+                }
+                
+                Button("Short Break") {
+                    vm.seconds = 5.0 * 60
+                }
+                Button("Long Break") {
+                    vm.seconds = 15.0 * 60
+                }
+            }
+            .buttonStyle(.bordered)
+            
+            
+            Text("\(vm.time)")
+                .font(.system(size: 100))
+                .alert("Time Complete", isPresented: $vm.showingAlert) {
+                    Button("Continue", role: .cancel) {
+                        // code
+                    }
+                }
+                
+
+            HStack {
+                
+                Button("Start") {
+                    vm.start(sec: vm.seconds)
+                }
+                .disabled(vm.isActive)
+                
+                Spacer()
+                
+                Button("Pause", role: .destructive) {
+                    vm.pause()
+                }
+                .disabled(!vm.isActive)
+                
+                
+                
+            }
             .padding()
+            .buttonStyle(.borderedProminent)
+            
+            Spacer()
+        }
+        .onReceive(timer) { _ in
+            vm.updateCountDown()
+        }
+        
     }
 }
 
---
+++ PomodoroStudy/ContentViewModel.swift
@@ -0,0 +1,68 @@
+//
+//  ContentViewModel.swift
+//  PomodoroStudy
+//
+//  Created by Joon Jang on 2022-08-17.
+//
+
+import Foundation
+
+// code help from:
+// https://www.youtube.com/watch?v=NAsQCNpodPI&list=LL&index=2
+extension ContentView {
+    final class ViewModel: ObservableObject {
+        @Published var isActive = false
+        @Published var showingAlert = false
+        @Published var time: String = "25:00"
+        @Published var seconds: Float = 25.0 * 60 {
+            didSet{
+                self.time = "\(Int(seconds)/60):00"
+            }
+        }
+        
+        private var initialTime = 0
+        private var endDate = Date()
+        
+        func start(sec: Float) {
+            self.initialTime = Int(seconds)
+            self.endDate = Date()
+            self.isActive = true
+            self.endDate = Calendar.current.date(byAdding: .second, value: Int(seconds), to: endDate)!
+        }
+        
+        func pause() {
+            self.isActive = false
+
+        }
+        
+        func reset() {
+            self.seconds = Float(initialTime)
+            self.isActive = false
+            self.time = "\(Int(seconds)/60):00"
+        }
+        
+        
+        
+        func updateCountDown() {
+            guard isActive else { return }
+            
+            let now = Date()
+            let diff = endDate.timeIntervalSince1970 - now.timeIntervalSince1970
+            
+            if diff <= 0{
+                self.isActive = false
+                self.time = "0:00"
+                self.showingAlert = true
+                return
+            }
+            
+            let date = Date(timeIntervalSince1970: diff)
+            let calendar = Calendar.current
+            let minutes = calendar.component(.minute, from: date)
+            let seconds = calendar.component(.second, from: date)
+            
+            self.seconds = Float(minutes * 60 + seconds)
+            self.time = String(format: "%d:%02d", minutes, seconds)
+        }
+    }
+}
